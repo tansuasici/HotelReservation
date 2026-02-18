ecd0d70aad195c65339cc05f6d7340c9b0d24e82
Tansu Aşıcı <gamex75369@gmail.com>
2026-02-18 15:39:46 +0300

    chore: remove obsolete CLI and GUI scripts, along with related configuration files

--- cli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Hotel Reservation CLI - connects to REST API server
-cd "$(dirname "$0")"
-java -cp "target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q)" \
-     hotel.reservation.cli.HotelReservationCLI
